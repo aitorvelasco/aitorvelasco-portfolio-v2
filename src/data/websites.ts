91297edcc9a0227dd1b0ef321fa521366f1c18b5
aitorvelasco <avelascoo92@gmail.com>
2026-02-21 13:02:13 +0100

    change mock project to real projects

--- src/data/websites.ts
+++ src/data/websites.ts
@@ -7,7 +7,13 @@ export interface Website {
   image: string;
   tags: string[];
   year: string;
+  status: WebsiteStatus;
   href?: string;
+}
+
+export enum WebsiteStatus {
+  completed = "Completado",
+  ongoing = "Ongoing"
 }
 
 export const websites: Website[] = [
@@ -19,48 +25,53 @@ export const websites: Website[] = [
     color: "from-rose-500 to-amber-500",
     image: "/websites/sm-foto.jpg",
     tags: ["HTML5", "CSS3", "JS"],
-    year: "2015"
+    year: "2015",
+    status: WebsiteStatus.completed
   },
   {
     id: "ecommerce-bq",
-    title: "Tienda BQ",
-    description: "Tienda online de BQ",
+    title: "Tienda bq",
+    description: "Tienda online de bq",
     category: "E-commerce",
     color: "from-emerald-500 to-cyan-500",
     image: "/websites/bq-store.png",
     tags: ["Magento", "jQuery"],
-    year: "2016"
+    year: "2016",
+    status: WebsiteStatus.completed
   },
   {
     id: "wordpress-diwo",
     title: "DIWO",
-    description: "Blog de DIWO para BQ",
+    description: "Blog de DIWO para bq",
     category: "Blog",
     color: "from-slate-500 to-zinc-500",
     image: "/websites/diwo.png",
     tags: ["Wordpress", "poedit"],
-    year: "2017"
+    year: "2017",
+    status: WebsiteStatus.completed
   },
   {
     id: "bq-website",
-    title: "BQ",
-    description: "Web oficial de la empresa de telefonñia española BQ",
+    title: "bq",
+    description: "Web oficial de la empresa de telefonia española bq",
     category: "website",
     color: "from-orange-500 to-yellow-500",
     image: "/websites/bqcom.png",
     tags: ["Twig", "SEO"],
-    year: "2018"
+    year: "2018",
+    status: WebsiteStatus.completed
   },
   {
     id: "llamaya-eshop",
-    title: "Llamaya eShop",
+    title: "llamaya eShop",
     description: "Tienda online de la marca llamaya",
     category: "eshop",
     color: "from-red-500 to-pink-500",
     image: "/websites/llamaya.png",
     tags: ["React", "NextJS", "Material UI"],
     year: "2019",
-    href: "https://www.llamaya.com"
+    href: "https://www.llamaya.com",
+    status: WebsiteStatus.ongoing
   },
   {
     id: "masmovil-eshop",
@@ -71,7 +82,8 @@ export const websites: Website[] = [
     image: "/websites/masmovil.png",
     tags: ["React", "NextJS", "Material UI"],
     year: "2020",
-    href: "https://www.masmovil.es"
+    href: "https://www.masmovil.es",
+    status: WebsiteStatus.ongoing
   },
   {
     id: "yoigo-eshop",
@@ -82,6 +94,7 @@ export const websites: Website[] = [
     image: "/websites/yoigo.png",
     tags: ["React", "NextJS", "Material UI"],
     year: "2021",
-    href: "https://www.yoigo.com"
+    href: "https://www.yoigo.com",
+    status: WebsiteStatus.ongoing
   }
 ];
