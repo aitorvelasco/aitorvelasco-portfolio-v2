a6067b1c411ba60a3c7c321456a9b520c8ad7b29
aitorvelasco <avelascoo92@gmail.com>
2026-02-22 03:29:08 +0100

    Deploy Astro to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aitorvelasco.github.io',
-  outDir: './docs',
   vite: {
     plugins: [tailwindcss()]
   }
